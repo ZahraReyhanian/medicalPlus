494c59abd8c65198c453482fad72999e8e90d7ed
Zahra Reyhanian <reyhanyanz@gmail.com>
2022-07-30 22:21:22 +0430

    add payment gateway

--- psychology/urls.py
+++ psychology/urls.py
@@ -1,7 +1,11 @@
 from rest_framework_nested import routers
+from django.urls import path
 from . import views
 
 router = routers.DefaultRouter()
 router.register('tests', views.TestViewSet, basename='tests')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('request/', views.send_request, name='request'),
+    path('verify/', views.verify , name='verify'),
+] + router.urls
