b6717a020a39d07afc2a90cc1d3ee57899a138cd
Zahra Reyhanian <reyhanyanz@gmail.com>
2022-08-10 11:42:00 +0430

    modify prod.py

--- medicalPlus/settings/prod.py
+++ medicalPlus/settings/prod.py
@@ -3,16 +3,16 @@ from .common import *
 
 DEBUG = False
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['medicalplus-prod.herokuapp.com', 'zahrarhn.pythonanywhere.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ['DATABASE_ENGINE'],
-        'NAME': os.environ['DATABASE_NAME'],
-        'HOST': os.environ['DATABASE_HOST'],
-        'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD']
+        'ENGINE': os.environ.get('DATABASE_ENGINE'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD')
     }
 }
